1f6f0776042aa32b0f548552039192bad15a0e55
Bruno Valle <brunolv6@gmail.com>
2019-06-28 00:02:20 -0300

    Alguns teste para fazer parte de cadastramento

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 document.querySelector("button").onclick = () => {
+    const nome = document.querySelector("#nome").value;
     const email = document.querySelector("#email").value;
     const senha = document.querySelector("#password").value;
-    if(email && senha){
+    if(email && senha && !nome){
         fetch("http://localhost:3000/", {
             method: "post",
             headers: {'Content-type': 'application/json'},
@@ -11,7 +12,40 @@ document.querySelector("button").onclick = () => {
                 senha: senha
             })
         }).then(() => alert("Cadastro Feito!"));
-    } else {
+    } /* else if(email && senha & !nome) {
+        fetch
+    } */ else {
         alert("Você precisa adicionar um email e uma senha para logar!");
     }
+}
+
+document.querySelector(".registrar").onclick = () => {
+    //trocando título
+    document.querySelector(".titulo").innerHTML = "Registrar";
+
+    //criar atributo type e dar seu valor
+    const ty = document.createAttribute("type");
+    ty.value = "text";
+    //criar atributo id e dar seu valor
+    const id = document.createAttribute("id");
+    id.value = "nome";
+    //criar atributo placeholder e dar seu valor
+    const pl = document.createAttribute("placeholder");
+    pl.value = "nome..";
+    //criar input e dar seus atributos
+    const input = document.createElement("input");
+    input.setAttributeNode(ty);
+    input.setAttributeNode(id);
+    input.setAttributeNode(pl);
+    //cria div
+    const di = document.createElement("div");
+    di.appendChild(input);
+
+    //add input no div correto
+    document.querySelector(".dados").insertBefore(di, document.querySelector("#firstDiv"));
+
+    //registrar ao final trocar por login
+    document.querySelector(".registrar").innerHTML = "Login";
+/*     document.querySelector(".registrar").classList.add(".login");
+    document.querySelector(".login").classList.remove(".registrar"); */
 }
